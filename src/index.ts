4f6fe20c87b3f4a601d2069d3354cf5cc1a75c35
d3d9ex <dencdamian@wp.pl>
2024-11-20 20:15:28 +0100

    Add fetching accounts and buying stocks

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,75 @@
 import dotenv from "dotenv";
-import puppeteer, {ElementHandle} from "puppeteer";
+import puppeteer, {Browser, ElementHandle, Page} from "puppeteer";
 
 dotenv.config();
 
-const run = async () => {
-  console.log("Starting...");
-  const debug = process.env.DEBUG === "true";
+interface BuyStockData {
+  symbol: string;
+  volume: number;
+  price: number;
+}
+
+const fillNumberInput = async (page: Page, selector: string, value: number) => {
+  const priceInput = await page.waitForSelector(selector);
+
+  const boundingBox = await priceInput!.boundingBox();
+  await page.mouse.click(
+    boundingBox!.x + boundingBox!.width / 2,
+    boundingBox!.y + boundingBox!.height / 2,
+    { count: 3 }
+  );
+  await priceInput!.press('Backspace');
+
+  await priceInput!.type(value.toString());
+}
+
+const buyStock = async (page: Page, data: BuyStockData) => {
+  const searchBarInput = await page.waitForSelector('.xs-symbol-search-bar-input-wrapper input.xs-symbol-search-bar-input');
+  await searchBarInput!.type(data.symbol);
+
+  const stockContainer = await page.waitForSelector(
+    '::-p-xpath(//div[contains(@class, "single-symbol-container") and ' +
+    '(.//span[contains(@class, "asset-class") and (text()="Stock" or text()="Akcje")]) and ' +
+    `.//p[contains(@class, "group-id") and starts-with(normalize-space(text()), "${data.symbol}")]])`
+  );
+
+  const orderButton = await stockContainer!.$('::-p-xpath(.//div[contains(@class, "mw-ct-ticket-btn-icon")])');
+  await orderButton!.click();
+
+  const stopLimitTab = await page.waitForSelector('ul.nav-tabs li[select="tabSelected(\'pending\')"] a');
+  await stopLimitTab!.click();
+
+  // volume must be set first because otherwise changing the price affects the volume input element
+  await fillNumberInput(page, 'div[data-id="volume.volume"] input[name="stepperInput"].xs-stepper-input', data.volume);
+  await fillNumberInput(page, 'div[data-id="openPrice"] input[name="stepperInput"].xs-stepper-input', data.price);
+
+  const buyLimitButton = await page.waitForFunction<unknown[], () => HTMLDivElement | false>(() => {
+    const buyLimitButton = document.querySelector<HTMLDivElement>('div.xs-popup-trade-button.xs-btn-buy')!;
+    return buyLimitButton.hasAttribute('disabled') ? false : buyLimitButton;
+  }) as ElementHandle<HTMLDivElement>;
+  await buyLimitButton!.click();
+
+  // uncomment to confirm the transaction
+  // const confirmButton = await page.waitForSelector('button#applyBtn');
+  // await confirmButton!.click();
+}
+
+const fetchAccounts = async (page: Page) => {
+  const accounts = await page.evaluate(() => {
+    const accountListElements = document.querySelectorAll('xs-combobox[title="Zmień konto"] .dropdown-menu li');
+
+    return Array.from(accountListElements).map((item) => {
+      const spans = item.querySelectorAll('span');
+      return Array.from(spans)
+        .map((span) => span.textContent!)
+        .join(' ');
+    });
+  });
+
+  console.log(accounts);
+}
+
+const handleXtb = async (browser: Browser) => {
   const email = process.env.EMAIL;
   const password = process.env.PASSWORD;
 
@@ -13,11 +77,8 @@ const run = async () => {
     throw new Error("EMAIL and PASSWORD must be set in environment variables.");
   }
 
-  const browser = await puppeteer.launch({
-    ...(debug ? {headless: false, slowMo: 10} : {}),
-  });
   const page = await browser.newPage();
-  const ua = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36";
+  const ua = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36";
   await page.setUserAgent(ua);
 
   await page.goto("https://xstation5.xtb.com/?branch=pl#/_/login");
@@ -52,7 +113,24 @@ const run = async () => {
 
   console.log(`Account value: ${accountValue}`);
 
-  !debug && await browser.close();
-};
+  await fetchAccounts(page);
+
+  await buyStock(page, {symbol: "META.US", price: 100, volume: 1});
+}
+
+const run = async () => {
+  console.log("Starting...");
+  const debug = process.env.DEBUG === "true";
+
+  const browser = await puppeteer.launch({
+    ...(debug ? { headless: false, slowMo: 10, defaultViewport: null } : {}),
+  });
+
+  try {
+    await handleXtb(browser);
+  } finally {
+    !debug && await browser.close();
+  }
+}
 
 run().catch(console.error);
